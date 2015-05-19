b0ae4f3bfda0e52d82cf85edbca6c0f2b7d91aa0
Jonathan Matthey <mattheyj@gmail.com>
2015-05-19 14:46:15 -0400

    update 200

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 //require the Twilio module and create a REST client
+var client = require('twilio')('AC18f37a0f522bb8bab1d36ea30274ff5c', 'a6f6c14b6187220176d780b2a125de55');
 
 var app = express();
 // app.use(express.urlencoded());
 
-app.get('/send-message', function(req, response) {
+app.get('/send-message', function(req, res) {
   var numb = req.query.to;
   var msg = req.query.msg;
 
@@ -17,7 +18,7 @@ app.get('/send-message', function(req, response) {
   //   from: '+12054028908', // A number you bought from Twilio and can use for outbound communication
   //   body: msg // body of the SMS message
 
-  // }, function(err, responseData) { //this function is executed when a response is received from Twilio
+  // }, function(err, responseData) { //this function is executed when a res is received from Twilio
 
   //   if (!err) { // "err" is an error received during the request, if any
 
@@ -26,11 +27,11 @@ app.get('/send-message', function(req, response) {
 
   //   } else {
   //     console.log(err);
-        // response.json({"result":"err","error":err});
+        // res.json({"result":"err","error":err});
   //   }
   // });
   //
-  response.json({"result":"success"});
+  res.sendStatus(200)
 
 });
 
